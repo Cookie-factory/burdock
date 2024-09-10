1b9bf6e4bd04a567d13439d9cb7184c63fcb8664
yunyami <yunyami0605@gmail.com>
2024-09-10 10:33:38 +0900

    feat: add login page ui
    1. login page 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import {View} from 'react-native';
-import {Button, Text} from 'react-native-paper';
+import {Text, TextInput} from 'react-native-paper';
+import CenterButton from '~/components/common/button/CenterButton';
 import useNavigate from '~/hooks/navigator/useNavigation';
 
 function Login() {
   const navigate = useNavigate();
   return (
     <View>
-      <Text>login</Text>
+      <View>
+        <TextInput
+          mode="outlined"
+          label="이메일"
+          placeholder="이메일"
+          right={<TextInput.Icon icon="eye" />}
+        />
 
-      <Button
-        mode="contained"
-        onPress={() => navigate.navigate('CommunityList')}>
-        Go Community
-      </Button>
+        <TextInput
+          mode="outlined"
+          label="비밀번호"
+          placeholder="비밀번호"
+          right={<TextInput.Icon icon="eye" />}
+        />
+      </View>
+
+      <View>
+        <CenterButton
+          style={{
+            width: 200,
+          }}
+          onPress={() => navigate.navigate('CommunityList')}>
+          <Text>이메일로 로그인</Text>
+        </CenterButton>
+      </View>
     </View>
   );
 }
