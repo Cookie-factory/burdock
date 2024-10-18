1afaf7ae96c6ab2e63f42984c754b19cebc319c7
yunyami <yunyami0605@gmail.com>
2024-10-18 11:02:48 +0900

    refactor: router 코드 수정
    1. 각 페이지 헤더 적용

--- src/router/BaseRouter.tsx
+++ src/router/BaseRouter.tsx
@@ -5,6 +5,8 @@ import CommunityContent from '~/pages/community/content';
 import CommunityRegister from '~/pages/community/register';
 import Login from '~/pages/login';
 import {RouteList} from '~/types/navigator';
+import Header from '~/components/common/header/Header';
+import BackButton from '~/components/common/button/BackButton';
 
 const stack = createNativeStackNavigator<RouteList>();
 
@@ -12,10 +14,44 @@ function BaseRouter() {
   return (
     <NavigationContainer>
       <stack.Navigator>
-        <stack.Screen name="Login" component={Login} />
-        <stack.Screen name="CommunityList" component={CommunityList} />
-        <stack.Screen name="CommunityRegister" component={CommunityRegister} />
-        <stack.Screen name="CommunityContent" component={CommunityContent} />
+        <stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: true,
+            header: () => <Header title="로그인" />,
+          }}
+        />
+        <stack.Screen
+          name="CommunityList"
+          component={CommunityList}
+          options={{
+            headerShown: true,
+            header: () => (
+              <Header title="커뮤니티" leftButton={<BackButton />} />
+            ),
+          }}
+        />
+        <stack.Screen
+          name="CommunityRegister"
+          component={CommunityRegister}
+          options={{
+            headerShown: true,
+            header: () => (
+              <Header title="게시글 등록" leftButton={<BackButton />} />
+            ),
+          }}
+        />
+        <stack.Screen
+          name="CommunityContent"
+          component={CommunityContent}
+          options={{
+            headerShown: true,
+            header: () => (
+              <Header title="게시글 내용" leftButton={<BackButton />} />
+            ),
+          }}
+        />
       </stack.Navigator>
     </NavigationContainer>
   );
