7b5d1fee06c55f7db3930e0454365a630084903f
yunyami <yunyami0605@gmail.com>
2024-11-07 15:55:36 +0900

    refactor: layout common component ui 수정
    1. border 제거

--- src/components/common/layout/InnerLayout.tsx
+++ src/components/common/layout/InnerLayout.tsx
@@ -6,7 +6,7 @@ import {CustomUIProps} from '~/types/style';
  *@description page inner layout 공통 컴포넌트
  */
 function InnerLayout(props: CustomUIProps) {
-  return <VStack flex={1} px={20} borderWidth={1} {...props} />;
+  return <VStack flex={1} px={20} {...props} />;
 }
 
 export default InnerLayout;
