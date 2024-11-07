09f2da81929d00dbdfb478f757a8de769331f9ef
yunyami <yunyami0605@gmail.com>
2024-11-07 17:07:26 +0900

    refactor: 공통 컴포넌트 input 변경
    1. 구조 변경 및 props -> 우측 자식 컴포넌트 추가

--- src/components/common/input/FormInput.tsx
+++ src/components/common/input/FormInput.tsx
@@ -1,13 +1,21 @@
-import React, {useState} from 'react';
+import React, {PropsWithChildren, useState} from 'react';
 import CustomInput from './Input';
 import {CustomUIProps, CustomViewStyle} from '~/types/style';
 import {TextInputProps} from 'react-native-paper';
 import {colors} from '~/constants/style';
 
+interface Props {
+  containerStyle?: CustomViewStyle;
+}
+
 /**
  *@description 폼 input 스타일 공통 컴포넌트
  */
-function FormInput(props: CustomUIProps<TextInputProps, CustomViewStyle>) {
+function FormInput(
+  props: PropsWithChildren<
+    CustomUIProps<TextInputProps, CustomViewStyle> & Props
+  >,
+) {
   // focus에 따른 input 스타일 변경 state
   const [calStyle, setCalStyle] = useState({
     borderWidth: 0,
@@ -34,11 +42,14 @@ function FormInput(props: CustomUIProps<TextInputProps, CustomViewStyle>) {
       pl={16}
       h={48}
       fontSize={16}
-      borderWidth={3 + calStyle.borderWidth}
       placeholderTextColor={colors.gray[40]}
-      borderColor={colors.mint[50]}
-      borderRadius={16}
       {...props}
+      containerStyle={{
+        ...props.containerStyle,
+        borderWidth: 3 + calStyle.borderWidth,
+        borderColor: colors.mint[50],
+        borderRadius: 14,
+      }}
     />
   );
 }
--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, {PropsWithChildren} from 'react';
 import {TextInput} from 'react-native';
 import {TextInputProps} from 'react-native-paper';
 import {CustomUIProps, CustomViewStyle} from '~/types/style';
 import {styleTransform} from '~/utils/style';
+import HStack from '../view/HStack';
+
+interface Props {
+  containerStyle?: CustomViewStyle;
+}
 
 /**
  *@description Input style button
  */
-function CustomInput(props: CustomUIProps<TextInputProps, CustomViewStyle>) {
+function CustomInput(
+  props: PropsWithChildren<
+    CustomUIProps<TextInputProps, CustomViewStyle> & Props
+  >,
+) {
   const transformStyle = styleTransform(props, {
-    borderWidth: 1,
     paddingLeft: 14,
     minHeight: 32,
     alignItems: 'center',
-    width: '100%',
+    flex: 1,
+    // borderWidth: 1,
   });
-  return <TextInput {...props} style={[transformStyle, props.style]} />;
+  return (
+    <HStack {...props.containerStyle}>
+      <TextInput {...props} style={[transformStyle, props.style]} />
+
+      {props.children}
+    </HStack>
+  );
 }
 
 export default CustomInput;
