d85901e9d24e4435f79697158dec3d89cf1b1c44
yunyami <yunyami0605@gmail.com>
2024-11-07 11:17:54 +0900

    feat: formInput 공용 컴포넌트 기능 추가
    1. 포커싱, 블러 이벤트에 따른 스타일 변경

--- src/components/common/input/FormInput.tsx
+++ src/components/common/input/FormInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CustomInput from './Input';
 import {CustomUIProps, CustomViewStyle} from '~/types/style';
 import {TextInputProps} from 'react-native-paper';
@@ -8,12 +8,36 @@ import {colors} from '~/constants/style';
  *@description 폼 input 스타일 공통 컴포넌트
  */
 function FormInput(props: CustomUIProps<TextInputProps, CustomViewStyle>) {
+  // focus에 따른 input 스타일 변경 state
+  const [calStyle, setCalStyle] = useState({
+    borderWidth: 0,
+    fontSize: 0,
+  });
+
   return (
     <CustomInput
+      onFocus={args => {
+        if (props.onFocus) {
+          props.onFocus(args);
+        }
+        setCalStyle({
+          borderWidth: 1,
+          fontSize: -2,
+        });
+      }}
+      onBlur={args => {
+        if (props.onBlur) {
+          props.onBlur(args);
+        }
+        setCalStyle({
+          borderWidth: 0,
+          fontSize: 0,
+        });
+      }}
       pl={16}
       h={48}
-      fontSize={16}
-      borderWidth={3}
+      fontSize={16 + calStyle.fontSize}
+      borderWidth={3 + calStyle.borderWidth}
       placeholderTextColor={colors.gray[40]}
       borderColor={colors.mint[50]}
       borderRadius={16}
