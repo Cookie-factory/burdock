155870a7ce5b2d700269628f438a6944992301a9
yun jong yun <yunyami0605@gmail.com>
2025-05-20 15:52:18 +0900

    chore: app push setting
    1. app push setting

--- App.tsx
+++ App.tsx
@@ -9,12 +9,61 @@ import {Provider} from 'react-redux';
 import {store} from '~/store';
 import BootSplash from 'react-native-bootsplash';
 const queryClient = new QueryClient();
+import messaging from '@react-native-firebase/messaging';
+import {Alert, PermissionsAndroid, Platform} from 'react-native';
 
 function App(): React.JSX.Element {
   console.log(config.TEST);
 
+  async function requestPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+
+    if (Platform.OS === 'android' && Platform.Version >= 33) {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      );
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   useEffect(() => {
     BootSplash.hide({fade: true});
+
+    requestPermission();
+
+    messaging()
+      .getToken()
+      .then(res => {
+        console.log('FCM Token:', res);
+      });
+
+    // 앱 종료 후 메시지 클릭
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log('App opened from quit state:', remoteMessage);
+        }
+      });
+
+    // 앱 백그라운드에서 메시지 클릭
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log('App opened from background state:', remoteMessage);
+    });
   }, []);
 
   return (
--- ios/burdock/AppDelegate.mm
+++ ios/burdock/AppDelegate.mm
@@ -1,11 +1,14 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h> // 추가
 #import <React/RCTBundleURLProvider.h>
 #import <RNKakaoLogins.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+   // 추가 - 상단에 작성 필요!
+  [FIRApp configure];
+
   self.moduleName = @"burdock";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
